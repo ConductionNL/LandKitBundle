ee41e5020b1f92a9583752b42fbea4a1ca69266d
Ruben van der Linde <ruben@conduction.nl>
2020-10-18 10:56:54 +0200

    First code

---
+++ LandKitBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+// Conduction/LandKitBundle/LandKitBundle.php
+
+/*
+ * This file is part of the Conduction Common Ground Bundle
+ *
+ * (c) Conduction <info@conduction.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Conduction\LandKitBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LandKitBundle extends Bundle
+{
+}
